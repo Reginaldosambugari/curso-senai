0709eeaeaefa8c24f8c951722473226739ceb334
Reginaldosambugari <105683118+Reginaldosambugari@users.noreply.github.com>
2022-05-30 20:43:30 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿
-Console.WriteLine ("Olá Mundo. Eu sou o Reginaldo");
-Console.WriteLine ("Meu primeiro programa c#");
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+Console.WriteLine("Digite um número?");
+num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0){
+Console.WriteLine("O número digitado é par!!");
+}else{}
+{
+    Console.WriteLine("O número digitado é impar");
+}
